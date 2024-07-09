a405dd2f5eb873dab1d348e5bedad328cc459d44
Rohit <rohit.kadu@ketto.org>
2024-07-09 23:42:16 +0530

    lazy loaded added

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { Genre, Movie } from "../types";
-import DefaultImg from '../assets/placeholder_img.png'
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
+import DefaultImg from '../assets/placeholder_img.png';
 
 interface MovieCardProps {
   movie: Movie;
@@ -8,28 +10,29 @@ interface MovieCardProps {
 }
 
 const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
-//   const genreList = movie?.genre_ids?.map((id: number) => {
-//     const findItem = genres && genres.find((genre: Genre) => genre.id === id);
-//     return findItem?.name;
-//   }).join(", ");
-  const posterPath = movie?.poster_path ? `https://image.tmdb.org/t/p/w500${movie?.poster_path}`:DefaultImg
+  const [isLoaded, setIsLoaded] = useState(false);
+  const posterPath = movie?.poster_path ? `https://image.tmdb.org/t/p/w500${movie?.poster_path}` : DefaultImg;
 
-//   console.log(movie?.genre_ids, genres, genreList);
+  const handleImageLoaded = () => {
+    setIsLoaded(true);
+  };
 
   return (
-    <div className="movie-card">
-      <img
-        src={posterPath}
-        alt={movie?.title}
-        className="movie-image"
-      />
-      {/* <div className="movie-desc">{genreList}</div> */}
+    <div className={`movie-card`}>
+      <div className="image-container">
+        {!isLoaded && <img src={DefaultImg} alt="placeholder" className="placeholder-image" />}
+        <LazyLoadImage
+          src={posterPath}
+          alt={movie?.title}
+          effect="blur"
+          placeholderSrc={DefaultImg}
+          className="movie-image"
+          afterLoad={handleImageLoaded}
+          visibleByDefault={!movie?.poster_path} // Show placeholder immediately if there's no poster path
+        />
+      </div>
       <div className="movie-info">
         <p className="movie-title">{movie?.title}</p>
-        {/* <p className="genre-list"><span>{genreList}</span></p> */}
-        {/* <p>Cast: {movie?.cast}</p>
-        <p>Director: {movie?.director}</p> */}
-        {/* <p>{movie.overview}</p> */}
       </div>
     </div>
   );
